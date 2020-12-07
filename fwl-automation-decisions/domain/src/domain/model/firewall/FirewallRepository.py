374614d3aecddc192741dbe13052febd726b267e
Alexandre Herculano <1170944@isep.ipp.pt>
2020-12-07 20:57:04 +0000

    added FirewallRepository to the domain layer

---
+++ fwl-automation-decisions/domain/src/domain/model/firewall/FirewallRepository.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+from domain.BaseRepository import BaseRepository
+from .Firewall import Firewall
+from .FirewallUUID import FirewallUUID
+
+
+class FirewallRepository(BaseRepository, ABC):
+
+    @abstractmethod
+    def add(self, o: Firewall) -> Firewall:
+        raise NotImplementedError
+
+    @abstractmethod
+    def list(self) -> [Firewall]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get(self, o: Firewall) -> Firewall:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_by_id(self, id: FirewallUUID) -> Firewall:
+        raise NotImplementedError
--- fwl-automation-decisions/domain/src/domain/model/firewall/__init__.py
+++ fwl-automation-decisions/domain/src/domain/model/firewall/__init__.py
@@ -2,3 +2,4 @@ from .Firewall import Firewall
 from .FirewallUUID import FirewallUUID
 from .FirewallName import FirewallName
 from .FirewallAccessLayer import FirewallAccessLayer
+from .FirewallRepository import FirewallRepository
